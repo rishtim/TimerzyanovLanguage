c1c2239ab7f691ffbe0ee4d698efcafc171e29f1
rishtim <russskiy.hyusten@gmail.com>
2024-12-12 14:31:47 +0500

    lab 25

---
+++ AddEditWindow.xaml.cs
@@ -0,0 +1,223 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TimerzyanovLanguage
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditWindow.xaml
+    /// </summary>
+    public partial class AddEditWindow : Window
+    {
+        private Client currentClient = new Client();
+
+        public bool check = false;
+        public AddEditWindow(Client client)
+        {
+            InitializeComponent();
+            if (client != null)
+            {
+                check = true;
+                currentClient = client;
+                BirthdayDate.Text = currentClient.Birthday.ToString();
+                if (currentClient.GenderCode == "м")
+                {
+                    GenderCodeBox.SelectedIndex = 0;
+                }
+                else
+                {
+                    GenderCodeBox.SelectedIndex = 1;
+                }
+
+            }
+            else
+            {
+                IDText.Visibility = Visibility.Hidden;
+                IDBox.Visibility = Visibility.Hidden;
+                GenderCodeBox.SelectedIndex = 0;
+            }
+            DataContext = currentClient;
+        }
+
+        private void ChangePictureBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog myOpenFileDialog = new OpenFileDialog();
+            if (myOpenFileDialog.ShowDialog() == true)
+            {
+                currentClient.PhotoPath = myOpenFileDialog.FileName;
+                PhotoImage.Source = new BitmapImage(new Uri(myOpenFileDialog.FileName));
+            }
+        }
+
+        private void ClientSave_Click(object sender, RoutedEventArgs e)
+        {
+            if (currentClient.ID == 0)
+            {
+                TimerzyanovLanguageEntities.GetContext().Client.Add(currentClient);
+            }
+            StringBuilder errors = new StringBuilder();
+            if (string.IsNullOrWhiteSpace(currentClient.LastName))
+                errors.AppendLine("Укажите фамилию!");
+            else
+            {
+                if (currentClient.LastName.Length > 50)
+                    errors.AppendLine("Слишком длинная фамилия!");
+            }
+            if (string.IsNullOrWhiteSpace(currentClient.FirstName))
+                errors.AppendLine("Укажите имя!");
+            else
+            {
+                if (currentClient.FirstName.Length > 50)
+                    errors.AppendLine("Слишком длинное имя!");
+            }
+            if (string.IsNullOrWhiteSpace(currentClient.Patronymic))
+                currentClient.Patronymic = null;
+            else
+            {
+                if (currentClient.Patronymic.Length > 50)
+                    errors.AppendLine("Слишком длинное отчество!");
+            }
+            if (string.IsNullOrWhiteSpace(currentClient.Email))
+                errors.AppendLine("Укажите Email!");
+            else
+            {
+                string emailPattern = @"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9]+$";
+                if (!Regex.IsMatch(currentClient.Email, emailPattern))
+                    errors.AppendLine("Не корректный Email!");
+            }
+            if (string.IsNullOrWhiteSpace(currentClient.Phone))
+                errors.AppendLine("Укажите номер телефона!");
+            else
+            {
+                if (currentClient.Phone.Count(c => c == '+') == 1)
+                {
+                    if (!(currentClient.Phone[0] == '+'))
+                        errors.AppendLine("Не корректный номер телефона!");
+                }
+                if (currentClient.Phone.Count(c => c == '+') > 1 || currentClient.Phone.Count(c => c == '(') > 1 || currentClient.Phone.Count(c => c == ')') > 1 || currentClient.Phone.Count(c => c == '-') > 2)
+                {
+                    errors.AppendLine("Не корректный номер телефона! ");
+                }
+            }
+            if (BirthdayDate.Text == "")
+            {
+                errors.AppendLine("Укажите дату рождения");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+
+            currentClient.Birthday = Convert.ToDateTime(BirthdayDate.Text);
+            if (GenderCodeBox.SelectedIndex == 0)
+            {
+                currentClient.GenderCode = "м";
+            }
+            else
+            {
+                currentClient.GenderCode = "ж";
+            }
+
+            List<Client> allClient = TimerzyanovLanguageEntities.GetContext().Client.ToList();
+            allClient = allClient.Where(p => p.FirstName == currentClient.FirstName && p.LastName == currentClient.LastName && p.Patronymic == currentClient.Patronymic && p.Phone == currentClient.Phone).ToList();
+            if (allClient.Count == 0 || check == true)
+            {
+                if (check == false)
+                {
+                    currentClient.RegistrationDate = DateTime.Now;
+                }
+                if (currentClient.ID == 0)
+                {
+                    TimerzyanovLanguageEntities.GetContext().Client.Add(currentClient);
+                }
+                try
+                {
+                    TimerzyanovLanguageEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Информация сохранена");
+                    this.Close();
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+            else
+                MessageBox.Show("Такой клиент уже существует");
+        }
+
+        private void PhoneBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string filteredText = new string(textBox.Text.Where(c => char.IsDigit(c) || c == '+' || c == '-' || c == '(' || c == ')' || c == ' ').ToArray());
+
+            if (filteredText != textBox.Text)
+            {
+                textBox.Text = filteredText;
+                textBox.SelectionStart = filteredText.Length;
+            }
+        }
+
+        private void EmailBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string filteredText = new string(textBox.Text.Where(c => char.IsLetterOrDigit(c) || c == '_' || c == '.' || c == '@').ToArray());
+
+            if (filteredText != textBox.Text)
+            {
+                textBox.Text = filteredText;
+                textBox.SelectionStart = filteredText.Length;
+            }
+        }
+
+        private void PatronymicBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string filteredText = new string(textBox.Text.Where(c => char.IsLetter(c) || c == ' ' || c == '-').ToArray());
+
+            if (filteredText != textBox.Text)
+            {
+                textBox.Text = filteredText;
+                textBox.SelectionStart = filteredText.Length;
+            }
+        }
+
+        private void LastNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string filteredText = new string(textBox.Text.Where(c => char.IsLetter(c) || c == ' ' || c == '-').ToArray());
+
+            if (filteredText != textBox.Text)
+            {
+                textBox.Text = filteredText;
+                textBox.SelectionStart = filteredText.Length;
+            }
+        }
+
+        private void FirstNameBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+            string filteredText = new string(textBox.Text.Where(c => char.IsLetter(c) || c == ' ' || c == '-').ToArray());
+
+            if (filteredText != textBox.Text)
+            {
+                textBox.Text = filteredText;
+                textBox.SelectionStart = filteredText.Length;
+            }
+        }
+    }
+}
---
+++ ClientPage.xaml.cs
@@ -0,0 +1,237 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TimerzyanovLanguage
+{
+    /// <summary>
+    /// Логика взаимодействия для ClientPage.xaml
+    /// </summary>
+
+    public partial class ClientPage : Page
+    {
+        List<Client> CurrentPageList = new List<Client>();
+        List<Client> TableList;
+        int CountRecords;
+        int CountPage;
+        int CurrentPage = 0;
+        int CountInPage;
+
+        public ClientPage()
+        {
+            InitializeComponent();
+            var currentClients = TimerzyanovLanguageEntities.GetContext().Client.ToList();
+            ClientListView.ItemsSource = currentClients;
+            PageComboBox.SelectedIndex = 0;
+            FiltrBox.SelectedIndex = 0;
+            SortBox.SelectedIndex = 0;
+            UpdateClients();
+        }
+
+        private void UpdateClients()
+        {
+            var currentClients = TimerzyanovLanguageEntities.GetContext().Client.ToList();
+            if (SortBox.SelectedIndex == 1)
+            {
+                currentClients = currentClients.OrderBy(p => p.FirstName).ToList();
+            }
+            else if (SortBox.SelectedIndex == 2)
+            {
+                currentClients = currentClients.OrderByDescending(p => DateTime.Parse((p.LastVisitDate.ToString() != "Нет") ? p.LastVisitDate.ToString() : "01.01.1991 09:00")).ToList();
+            }
+            else if (SortBox.SelectedIndex == 3)
+            {
+                currentClients = currentClients.OrderByDescending(p => p.VisitCount).ToList();
+            }
+
+            if (FiltrBox.SelectedIndex == 1)
+            {
+                currentClients = currentClients.Where(p => p.GenderCode == "ж").ToList();
+            }
+            else if (FiltrBox.SelectedIndex == 2)
+            {
+                currentClients = currentClients.Where(p => p.GenderCode == "м").ToList();
+            }
+
+            currentClients = currentClients.Where(p => p.LastName.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.FirstName.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Patronymic.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Email.ToLower().Contains(TBoxSearch.Text.ToLower()) || p.Phone.Replace("+", "").Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "").ToLower().Contains(TBoxSearch.Text.Replace("+", "").Replace(" ", "").Replace("-", "").Replace("(", "").Replace(")", "").ToLower())).ToList();
+
+            CountInPage = 10;
+            ClientListView.ItemsSource = currentClients;
+            TableList = currentClients;
+            if (PageComboBox.SelectedIndex == 0)
+                CountInPage = 10;
+            if (PageComboBox.SelectedIndex == 1)
+                CountInPage = 50;
+            if (PageComboBox.SelectedIndex == 2)
+                CountInPage = 200;
+            if (PageComboBox.SelectedIndex == 3)
+                CountInPage = TableList.Count;
+            ChangePage(0, 0);
+        }
+
+        private void PageComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+
+        private void PageListBox_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            ChangePage(0, Convert.ToInt32(PageListBox.SelectedItem.ToString()) - 1);
+        }
+
+        private void LeftDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(1, null);
+        }
+
+        private void RightDirButton_Click(object sender, RoutedEventArgs e)
+        {
+            ChangePage(2, null);
+        }
+
+        private void ChangePage(int direction, int? selectedPage)
+        {
+            CurrentPageList.Clear();
+            CountRecords = TableList.Count;
+
+            if (CountRecords % CountInPage > 0)
+            {
+                CountPage = CountRecords / CountInPage + 1;
+            }
+            else
+            {
+                CountPage = CountRecords / CountInPage;
+            }
+
+            Boolean Ifupdate = true;
+
+            int min;
+
+            if (selectedPage.HasValue)
+            {
+                if (selectedPage >= 0 && selectedPage <= CountPage)
+                {
+                    CurrentPage = (int)selectedPage;
+                    min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                    for (int i = CurrentPage * CountInPage; i < min; i++)
+                    {
+                        CurrentPageList.Add(TableList[i]);
+                    }
+                }
+            }
+            else
+            {
+                switch (direction)
+                {
+                    case 1:
+                        if (CurrentPage > 0)
+                        {
+                            CurrentPage--;
+                            min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                            for (int i = CurrentPage * CountInPage; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+
+                    case 2:
+                        if (CurrentPage < CountPage - 1)
+                        {
+                            CurrentPage++;
+                            min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                            for (int i = CurrentPage * CountInPage; i < min; i++)
+                            {
+                                CurrentPageList.Add(TableList[i]);
+                            }
+                        }
+                        else
+                        {
+                            Ifupdate = false;
+                        }
+                        break;
+                }
+            }
+            if (Ifupdate)
+            {
+                PageListBox.Items.Clear();
+
+                for (int i = 1; i <= CountPage; i++)
+                {
+                    PageListBox.Items.Add(i);
+                }
+                PageListBox.SelectedIndex = CurrentPage;
+
+                min = CurrentPage * CountInPage + CountInPage < CountRecords ? CurrentPage * CountInPage + CountInPage : CountRecords;
+                CurrentCount.Text = min.ToString();
+                AllCount.Text = " из " + CountRecords.ToString();
+
+                ClientListView.ItemsSource = CurrentPageList;
+                ClientListView.Items.Refresh();
+
+            }
+        }
+
+        private void BtnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            var currentClient = (sender as Button).DataContext as Client;
+
+            if (currentClient.VisitCount == 0)
+            {
+                if (MessageBox.Show("Вы точно хотите выполнить удаление?", "Внимание!", MessageBoxButton.YesNo, MessageBoxImage.Question) == MessageBoxResult.Yes)
+                {
+                    TimerzyanovLanguageEntities.GetContext().Client.Remove(currentClient);
+                    TimerzyanovLanguageEntities.GetContext().SaveChanges();
+                    ClientListView.ItemsSource = TimerzyanovLanguageEntities.GetContext().Client.ToList();
+                    UpdateClients();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Невозможно выполнить удаление, так как у клиента есть посещения!");
+            }
+        }
+
+        private void TBoxSearch_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+
+        private void FiltrBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+
+        private void SortBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            UpdateClients();
+        }
+
+        private void BtnAddEdit_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditWindow((sender as Button).DataContext as Client).ShowDialog();
+            UpdateClients();
+        }
+
+        private void AddClient_Click(object sender, RoutedEventArgs e)
+        {
+            new AddEditWindow(null).ShowDialog();
+            UpdateClients();
+        }
+    }
+}
